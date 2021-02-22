f99a2be227b1fa51dc60471691cd898eb00ea911
Paweł Burgchardt <pawel.burgchardt@gmail.com>
2021-02-23 00:18:16 +0100

    testing staging cI/CI

--- chess/chess/index.js
+++ chess/chess/index.js
@@ -29,7 +29,7 @@ RefreshMoves = () => {
 };
 
 PerformGolemCalculationsWrapper = async function (moveData) {
-  moveData.depth = moveData.turnId == "w" ? 3 : 3;
+  moveData.depth = moveData.turnId == "w" ? 5 : 6;
 
   moveData.taskId = gethTaskIdHash(moveData.gameId, moveData.gameStep);
 
